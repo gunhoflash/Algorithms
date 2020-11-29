6781f07c37b74756db41fb30a34eb8c4da986586
Geonho Kim <gunhoflash@gmail.com>
2020-11-30 00:31:35 +0900

    Solve: 8980

---
+++ BOJ/8980.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#define min(a, b) (a < b ? a : b)
+
+using namespace std;
+
+struct Request {
+	int from;
+	int to;
+	int n_box;
+};
+
+bool compare_request(const Request &a, const Request &b) {
+	if (a.to != b.to) {
+		return a.to < b.to;
+	} else {
+		return a.from > b.from;
+	}
+}
+
+int main(void) {
+	int
+		i, j, // indexers
+		N, // the number of villages
+		C, // capacity of truck
+		M, // the number of request
+		from, to, max_deliverable,
+		delivered_boxes; // the number of delivered boxes
+
+	vector<int>
+		carriable;
+
+	vector<Request>
+		requests;
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// get the number of villages and capacity of the truck
+	cin >> N >> C;
+	carriable.resize(N, C);
+
+	// get all requests
+	cin >> M;
+	requests.resize(M);
+	for (i = 0; i < M; i++) {
+		cin >> requests[i].from >> requests[i].to >> requests[i].n_box;
+	}
+
+	// sort all requests
+	sort(requests.begin(), requests.end(), compare_request);
+
+	// check all requests in order
+	delivered_boxes = 0;
+	for (i = 0; i < M; i++) {
+		from = requests[i].from;
+		to = requests[i].to;
+		max_deliverable = min(requests[i].n_box, C);
+
+		// check the maximum number of deliverable boxes
+		for (j = from; j < to; j++) {
+			max_deliverable = min(max_deliverable, carriable[j]);
+		}
+
+		// deliver some boxes
+		if (max_deliverable > 0) {
+			for (j = from; j < to; j++) {
+				carriable[j] -= max_deliverable;
+			}
+			delivered_boxes += max_deliverable;
+		}
+	}
+
+	cout << delivered_boxes;
+
+	return 0;
+}
