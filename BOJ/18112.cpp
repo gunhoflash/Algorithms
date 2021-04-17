7f216dcad8a8796c948996ac0d41e6a6c14b1863
dewy.i <dewy.i@kakaocorp.com>
2021-04-17 17:31:52 +0900

    Separate main function

--- BOJ/18112.cpp
+++ BOJ/18112.cpp
@@ -1,28 +1,44 @@
+#define UPPER_LIMIT 0b10000000000
+#define LOWER_LIMIT 1
+
 #include <iostream>
 #include <map>
 #include <vector>
 
-#define is_exist(map, value) (map.find(value) != map.end())
-
 using namespace std;
 
-int to_binary(int n) {
+inline int to_binary(int n) {
 	int p = 1, binary = 0;
 	while (n) {
 		if (n % 10) binary += p;
 		p <<= 1;
 		n /= 10;
 	}
-
 	return binary;
 }
+
+inline bool is_exist(map<int, int> &map, int value) {
+	return map.find(value) != map.end();
+}
+
+inline void clear_swap(vector<int> &from, vector<int> &to) {
+	to.clear();
+	to.insert(to.end(), from.begin(), from.end());
+	from.clear();
+}
+
+inline void check_and_assign(int next, int n_operation, map<int, int> &min_n_operation, vector<int> &next_visiting) {
+	if (next <= UPPER_LIMIT && next >= LOWER_LIMIT && !is_exist(min_n_operation, next)) {
+		min_n_operation.insert({ next, n_operation });
+		next_visiting.push_back(next);
+	}
+}
+
 
 int main(void) {
 	int
 		L, K,
-		n_operation,
-		upper_limit = 1 << 10,
-		lower_limit = 1;
+		n_operation;
 
 	map<int, int> min_n_operation;
 	vector<int> now_visiting, next_visiting;
@@ -48,45 +64,19 @@ int main(void) {
 			// operation 1
 			bool msb_found = false;
 			for (int i = 10; i >= 0; i--) {
-				if (msb_found) {
-					int next = now ^ (1 << i);
-					if (next > upper_limit || next < lower_limit) continue;
-					if (!is_exist(min_n_operation, next)) {
-						// new value found
-						min_n_operation.insert({ next, n_operation });
-						next_visiting.push_back(next);
-					}
-				} else msb_found |= ((1 << i) & now);
+				if (msb_found)
+					check_and_assign(now ^ (1 << i), n_operation, min_n_operation, next_visiting);
+				else
+					msb_found |= (1 << i) & now;
 			}
 
-			// operation 2
-			{
-				int next = now + 1;
-				if (next > upper_limit || next < lower_limit) continue;
-				if (!is_exist(min_n_operation, next)) {
-					// new value found
-					min_n_operation.insert({ next, n_operation });
-					next_visiting.push_back(next);
-				}
-			}
-
-			// operation 3
-			{
-				int next = now - 1;
-				if (next > upper_limit || next < lower_limit) continue;
-				if (!is_exist(min_n_operation, next)) {
-					// new value found
-					min_n_operation.insert({ next, n_operation });
-					next_visiting.push_back(next);
-				}
-			}
+			// operation 2, 3
+			check_and_assign(now + 1, n_operation, min_n_operation, next_visiting);
+			check_and_assign(now - 1, n_operation, min_n_operation, next_visiting);
 		}
 
-
 		// swap vectors
-		now_visiting.clear();
-		now_visiting.insert(now_visiting.end(), next_visiting.begin(), next_visiting.end());
-		next_visiting.clear();
+		clear_swap(next_visiting, now_visiting);
 	}
 
 	// print the answer
