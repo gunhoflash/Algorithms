b9bb9a9a89edf31f15a4c5cd55f6f6cc77d83d5b
dewy.i <dewy.i@kakaocorp.com>
2021-04-25 17:45:17 +0900

    Solve: 10816

---
+++ BOJ/10816.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(void) {
+	int N, M;
+	map<int, int> deck;
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	while (N--) {
+		int n;
+		cin >> n;
+		deck[n]++;
+	}
+
+	cin >> M;
+	while (M--) {
+		int n;
+		cin >> n;
+		cout << deck[n] << ' ';
+	}
+
+	return 0;
+}
