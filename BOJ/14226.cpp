9755c4a71253da07b7a2c5bddc698c27b75bbdd4
dewy.i <dewy.i@kakaocorp.com>
2021-05-20 02:06:09 +0900

    Delete duplicated code

--- BOJ/14226.cpp
+++ BOJ/14226.cpp
@@ -10,6 +10,13 @@ struct State {
 	int cost;
 };
 
+inline void visit_next(int value, int clipboard, int cost, queue<State> &visiting_queue, vector<vector<bool>> &is_visited) {
+	if (!is_visited[value][clipboard]) {
+		is_visited[value][clipboard] = true;
+		visiting_queue.push({ value, clipboard, cost });
+	}
+}
+
 int main(void) {
 	int S;
 	vector<vector<bool>> is_visited;
@@ -22,10 +29,11 @@ int main(void) {
 
 	while (true) {
 		// pop one
-		int next_value;
-		int value = visiting_queue.front().value;
-		int clipboard = visiting_queue.front().clipboard;
-		int cost = visiting_queue.front().cost;
+		int
+			next_value,
+			value = visiting_queue.front().value,
+			clipboard = visiting_queue.front().clipboard,
+			cost = visiting_queue.front().cost;
 		visiting_queue.pop();
 
 		// end of searching
@@ -36,40 +44,19 @@ int main(void) {
 
 		// method 1
 		if (clipboard != value && value < S) {
-			if (!is_visited[value][value]) {
-				is_visited[value][value] = true;
-				visiting_queue.push({
-					value,
-					value,
-					cost + 1
-				});
-			}
+			visit_next(value, value, cost + 1, visiting_queue, is_visited);
 		}
 
 		// method 2
 		next_value = value + clipboard;
 		if (next_value < S * 2) {
-			if (!is_visited[next_value][clipboard]) {
-				is_visited[next_value][clipboard] = true;
-				visiting_queue.push({
-					next_value,
-					clipboard,
-					cost + 1
-				});
-			}
+			visit_next(next_value, clipboard, cost + 1, visiting_queue, is_visited);
 		}
 
 		// method 3
 		next_value = value - 1;
 		if (next_value >= 0) {
-			if (!is_visited[next_value][clipboard]) {
-				is_visited[next_value][clipboard] = true;
-				visiting_queue.push({
-					next_value,
-					clipboard,
-					cost + 1
-				});
-			}
+			visit_next(next_value, clipboard, cost + 1, visiting_queue, is_visited);
 		}
 	}
 
