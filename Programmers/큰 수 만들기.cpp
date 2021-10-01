5ce149924ce031df406a2705afc871a3a6803e62
dewy.i <dewy.i@kakaocorp.com>
2021-10-02 04:28:02 +0900

    Solve: 큰 수 만들기

--- Programmers/큰 수 만들기.cpp
+++ Programmers/큰 수 만들기.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 string solution(string number, int k) {
-	int i, size;
+	int i = 0;
+	int size = number.size();
 
-	while (k > 0) {
-		size = number.size();
-		for (i = 0; i < size - 1; i++) {
+	while (k && i < size - 1) {
+		if (number[i] < number[i + 1]) {
 			// delete one character
-			if (number[i] < number[i + 1]) {
-				number.erase(i, 1);
-				k--;
-				break;
-			}
+			number.erase(i, 1);
+			if (i > 0) i--;
+			k--;
+		} else {
+			i++;
 		}
+	}
 
-		// if no character deleted, delete the last
-		if (i >= size - 1) {
-			number.erase(size - k);
-			k = 0;
-		}
+	// if no character deleted, delete the last
+	if (k) {
+		number.erase(size - k);
 	}
 
 	return number;
