fa9c63e396130fadbbf16e1ace5cf80877e15634
gunhoflash <gunhoflash@gmail.com>
2023-04-30 16:06:39 +0900

    Edit template.ts

---
+++ BOJ/template.ts
@@ -0,0 +1,9 @@
+import * as fs from 'fs';
+
+const inputManager: Generator<string, any, unknown> = (function *() {
+  const stdin = '/dev/stdin';
+  const lines = fs.readFileSync(stdin).toString().split('\n');
+  for (const line of lines) yield line;
+  return null;
+})();
+const nextLine = (): string => inputManager.next().value;
